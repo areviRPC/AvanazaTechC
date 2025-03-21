ab4424fe0a5cfa35bc82422e333fd503e5118ee2
areviRPC <csebastian.riverap@gmail.com>
2025-03-21 16:02:38 -0500

    recetas

---
+++ CursoC/22- Recetas.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    // lista de recetas
+    char* recipes[] = {"bread","sandwich"};
+    int recipesSize = sizeof(recipes)/sizeof(recipes[0]);
+
+
+    // lista de ingredientes
+    char* ingredients[] = {{"yeast","flour"},{"bread","meat"},{"algo mas"}};
+    int ingredientsSize =  (sizeof(ingredients)/(sizeof(ingredients[0][0]) * sizeof(ingredients[0])) );
+    ;
+    // tamaño de los subelementos
+    int* ingredientsColSize;
+
+    // lista de suministros
+    char* supplies[] = {"yeast","flour","meat"};
+    int suppliesSize = (sizeof(supplies)/sizeof(supplies[0]));
+
+    // tamaño de la lista de creaciones posibles 
+    int* returnSize;
+
+    printf("tamaño recetas: %d ingredientes: %d, elementos: %d \n", recipesSize , ingredientsSize , suppliesSize);
+
+
+    return 0;
+}
